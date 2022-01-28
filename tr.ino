984ab9e0fbcdec04131113bc05c028d00b389573
edith-aniesheinemann-nti-johanneberg <edith.aniesheinemann@elev.ga.ntig.se>
2022-01-28 09:13:02 +0100

    Co-authored-by: emma-gran-nti-johanneberg <emma-gran-nti-johanneberg@users.noreply.github.com>

--- tr.ino
+++ tr.ino
@@ -1,3 +1,31 @@
+/*
+ keyestudio 4wd BT Car V2
+ lesson 6.1
+ IRremote
+ http://www.keyestudio.com
+*/ 
+// Koden nedan tror EMMA är till för att testa IRremote och fjärkontrollen
+
+#include <IRremote.h>     // IRremote library statement
+int RECV_PIN = A0;        //define the pins of IR receiver as A0
+IRrecv irrecv(RECV_PIN);   
+decode_results results;   // decode results exist in the“result” of “decode results”
+void setup()  
+	{  
+      Serial.begin(9600);  
+      irrecv.enableIRIn(); // Enable receiver
+	}  
+ void loop() {  
+	  if (irrecv.decode(&results))//decode successfully, receive a set of infrared signals
+	  {  
+	    Serial.println(results.value, HEX);//Wrap word in 16 HEX to output and receive code 
+	    irrecv.resume(); // Receive the next value
+	  }  
+	  delay(100);  
+	} //*******************************************************
+
+
+  khni
 
 /*
 intL_pin=6; //pins of left line trackingsensor
@@ -26,6 +54,5 @@ void loop(){
     delay(500); //delay in between reads for stability
 }
 *\
-
 
 
